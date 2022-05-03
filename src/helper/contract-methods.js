289489080049c4f2abc816dab2aa67bc61f4b26f
surajsingh <surajt9755@gmail.com>
2022-05-03 11:41:19 +0400

    feat: project setup

---
+++ src/helper/contract-methods.js
@@ -0,0 +1,23 @@
+import { ethers } from "ethers";
+import aggregratorV3Interface from "../contracts/aggregatorV3Interfaceabi.json";
+
+export const getContractInstance = (address, abi, signer) => {
+  return new ethers.Contract(address, abi, signer);
+};
+
+export const getPriceFeeds = async (coinPairAddress) => {
+  const aggregatorV3InterfaceABI = aggregratorV3Interface.abi;
+  const provider = new ethers.providers.JsonRpcProvider(
+    `https://polygon-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_MATIC_MAINNET_KEY}`
+  );
+  const priceFeed = getContractInstance(
+    coinPairAddress,
+    aggregatorV3InterfaceABI,
+    provider
+  );
+  let roundData = await priceFeed.latestRoundData();
+  let decimals = await priceFeed.decimals();
+  return Number(
+    (roundData.answer.toString() / Math.pow(10, decimals)).toFixed(2)
+  );
+};
