6003e2f9f2cc0707024ed80f6fb7e57e3c5bcd5b
surajsingh <surajt9755@gmail.com>
2022-05-09 07:52:09 +0400

    Feat: Contract and UI updates

--- src/helper/contract-methods.js
+++ src/helper/contract-methods.js
@@ -23,7 +23,7 @@ export const getPriceFeeds = async (coinPairAddress) => {
 };
 
 export const getRoundsInfo = async (contract, pair) => {
-  return await contract.getPairRounds(pair);
+  return await contract.getPairRounds(pair, 5);
 };
 
 export const getPairRounds = async (contract, pair) => {
@@ -32,35 +32,4 @@ export const getPairRounds = async (contract, pair) => {
 
 export const claimReward = async (contract, pair, pairRound) => {
   return await contract.claimReward(pairRound, pair);
-};
-
-export const getUsersData = async (contract, allRounds, setUsers) => {
-  const data = await getPairRounds(contract, 1);
-  if (data?.length === 2) {
-    const usersData = data[0]?.reduce((acc, user, index) => {
-      const roundNumber = data[1][index]?.toNumber();
-      if (!allRounds?.[roundNumber - 1]) return;
-      const { firstTokenPriceChange, secondTokenPriceChange } =
-        allRounds[roundNumber - 1];
-
-      let isWinner = false;
-
-      const userTokenSelectedChoice = user?.tokenselected;
-      if (firstTokenPriceChange !== secondTokenPriceChange) {
-        const tokenDominance =
-          firstTokenPriceChange > secondTokenPriceChange ? 0 : 1;
-        isWinner = userTokenSelectedChoice === tokenDominance;
-      }
-
-      acc[roundNumber] = {
-        amount: user?.amount.toNumber(),
-        tokenSelected: user?.tokenselected,
-        claim: user?.claim,
-        isWinner,
-      };
-      return acc;
-    }, {});
-
-    setUsers(usersData);
-  }
 };
